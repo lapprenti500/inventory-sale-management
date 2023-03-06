7378c2a8630bf5afe94f096e4ca34dc475916b41
amdy10 <amdy.dioum@univ-thies.sn>
2023-03-06 10:51:42 +0000

    Add Font awesome In Project

--- resources/views/admin_dashboard.blade.php
+++ resources/views/admin_dashboard.blade.php
@@ -33,6 +33,10 @@
           <link href="{{ asset('backend/assets/libs/datatables.net-select-bs5/css//select.bootstrap5.min.css') }}" rel="stylesheet" type="text/css" />
           <!-- dataTables end -->
 
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+           integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+           crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     </head>
 
     <!-- body start -->
