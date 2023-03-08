18a5af9753541426ac33dfb84e5ac05338925e99
amdy10 <amdy.dioum@univ-thies.sn>
2023-03-08 11:11:14 +0000

    Gestion des produits

--- resources/views/admin_dashboard.blade.php
+++ resources/views/admin_dashboard.blade.php
@@ -127,6 +127,6 @@
 
      <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
      <script src="{{ asset('backend/assets/js/code.js') }}"></script>
-
+     <script src="{{ asset('backend/assets/js/validate.min.js') }}"></script>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\CustomerController;
 use App\Http\Controllers\Backend\SupplierController;
 use App\Http\Controllers\Backend\CategoryController;
+use App\Http\Controllers\Backend\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -85,6 +86,20 @@ Route::controller(CategoryController::class)->group(function(){
     Route::get('/edit/category/{id}','EditCategory')->name('edit.category');
     Route::post('/update/category','UpdateCategory')->name('category.update');
     Route::get('/delete/category/{id}','DeleteCategory')->name('delete.category');
+
+    });
+
+///Product All Route
+Route::controller(ProductController::class)->group(function(){
+
+    Route::get('/all/product','AllProduct')->name('all.product');
+    Route::get('/add/product','AddProduct')->name('add.product');
+    Route::post('/store/product','StoreProduct')->name('product.store');
+    Route::get('/edit/product/{id}','EditProduct')->name('edit.product');
+    Route::post('/update/product','UdateProduct')->name('product.update');
+    Route::get('/delete/product/{id}','DeleteProduct')->name('delete.product');
+
+    Route::get('/barcode/product/{id}','BarcodeProduct')->name('barcode.product');
 
     });
 
