4c78fdb221f962bf776322552f5f589e935e377d
amdy10 <amdy.dioum@univ-thies.sn>
2023-03-08 11:00:00 +0000

    Categorie

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\CustomerController;
 use App\Http\Controllers\Backend\SupplierController;
+use App\Http\Controllers\Backend\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -76,5 +77,15 @@ Route::controller(SupplierController::class)->group(function(){
     Route::get('/details/supplier/{id}','DetailsSupplier')->name('details.supplier');
     });
 
+///Category All Route
+Route::controller(CategoryController::class)->group(function(){
+
+    Route::get('/all/category','AllCategory')->name('all.category');
+    Route::post('/store/category','StoreCategory')->name('category.store');
+    Route::get('/edit/category/{id}','EditCategory')->name('edit.category');
+    Route::post('/update/category','UpdateCategory')->name('category.update');
+    Route::get('/delete/category/{id}','DeleteCategory')->name('delete.category');
+
+    });
 
     ?>
